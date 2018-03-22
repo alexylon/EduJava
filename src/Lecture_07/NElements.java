06388ce09149d6078feea4b9d2718e3a21b3ef4a
alexylon <euax@icloud.com>
2018-03-22 17:03:36 +0200

    new

---
+++ src/Lecture_07/NElements.java
@@ -0,0 +1,26 @@
+package Lecture_07;
+
+import java.util.Scanner;
+
+public class NElements {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter how many elements: ");
+        int n = sc.nextInt();
+        int[] array = new int[n];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Enter element #" + (i + 1) + ": ");
+            array[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < (array.length - 1); i++) {
+            if (array[i] == array[i + 1]) {
+                System.out.print(array[i]);
+            }
+        }
+    }
+
+}
