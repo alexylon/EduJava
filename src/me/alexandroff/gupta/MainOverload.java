b310688799f8eab9f3c88c5de49d207b7842ef1f
alexylon <euax@icloud.com>
2018-06-15 08:55:26 +0300

    asus

---
+++ src/me/alexandroff/gupta/MainOverload.java
@@ -0,0 +1,17 @@
+package me.alexandroff.gupta;
+
+public class MainOverload {
+
+    public static void main(String args) {
+        System.out.println("Hello exam 2");
+    }
+
+    public static void main(String args[]) {
+        System.out.println("Hello exam");
+        main(5);
+    }
+
+    public static void main(int number) {
+        System.out.println("Hello exam 3: " + number);
+    }
+}
