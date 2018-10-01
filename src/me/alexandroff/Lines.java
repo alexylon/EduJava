d516d6b9a46a8263d83f1fd5be7832a6430d39d9
alexylon <euax@icloud.com>
2018-10-01 13:03:49 +0300

    irena

---
+++ src/me/alexandroff/Lines.java
@@ -0,0 +1,13 @@
+package me.alexandroff;
+
+public class Lines {
+
+    public static void line1() {
+        System.out.println();
+    }
+
+    public static void line2() {
+        System.out.println();
+        System.out.println();
+    }
+}
