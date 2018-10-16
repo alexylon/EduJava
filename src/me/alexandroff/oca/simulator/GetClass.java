358337fa6b57b02f9fca16773f83d28874d63df8
alexylon <euax@icloud.com>
2018-10-16 17:08:16 +0300

    asus

--- src/me/alexandroff/oca/simulator/GetClass.java
+++ src/me/alexandroff/oca/simulator/GetClass.java
@@ -1,20 +1,18 @@
 package me.alexandroff.oca.simulator;
 
-class A {
+class E {
 }
 
-class AA extends A {
+class EE extends E {
 }
 
 
 class TestClass3 {
     public static void main(String[] args) throws Exception {
-        A a = new A();
-        AA aa = new AA();
+        E a = new E();
+        EE aa = new EE();
         a = aa;
         System.out.println("a = " + a.getClass());
         System.out.println("aa = " + aa.getClass());
-
-
     }
 }
--- src/me/alexandroff/oca/simulator/Test08.java
+++ src/me/alexandroff/oca/simulator/Test08.java
@@ -1,36 +1,10 @@
 package me.alexandroff.oca.simulator;
 
-public class Test08 {
-    public static void main(String[] args) {
-
-        Integer i1 = 1;
-        Integer i2 = new Integer(1);
-        int i3 = 1;
-        Byte b1 = 1;
-        Long l1 = 1L;
-
-        System.out.println(i1 == i2);//false
-        System.out.println(i1 == i3);//true
-        //because one operand is a primitive int and so the other
-        // will be unboxed and then the value will be compared
-        /*
-        System.out.println(i1 == b1); - Won't compile
-        System.out.println(i1 == l1); - Won't compile
-        */
-        //because type of i1 and b1 references are classes
-        //that are not in the same class hierarchy
-
-        System.out.println(i1.equals(i2));//true
-        System.out.println(i1.equals(i3));//true
-        System.out.println(i1.equals(b1));//false
-        System.out.println(i1.equals(l1));//false
-    }
+interface X1{ }
+interface X2{ }
+class A { }
+class B extends A implements X1{ }
+class C extends B implements X2{
+    D d = new D();
 }
-
-/*
-Signature of equals method is : 'boolean equals(Object o)';
-So it can take any object hence there will be no compilation error.
-Further, The equals methods of all wrapper classes first check if
-the two object are of same class or not.
-If not, they immediately return false.
- */
+class D { }
