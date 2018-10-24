adc81edb603467f7388377213bbf7722520a6508
alexylon <euax@icloud.com>
2018-10-24 16:32:45 +0300

    asus

--- src/me/alexandroff/oca/simulator/Test05.java
+++ src/me/alexandroff/oca/simulator/Test05.java
@@ -8,5 +8,15 @@ public class Test05 {
         Boolean b = false;
         if (i.equals(b)) System.out.println("equal");   // 1
         else System.out.println("not equal");
+
+        System.out.println(5 % 3);
+        System.out.println(3 % 3);
+        System.out.println(7 % 3);
+
+        try {
+            System.out.println("e");
+            throw new IllegalArgumentException();
+        } catch (RuntimeException e){}
+        System.out.println("f");
     }
 }
