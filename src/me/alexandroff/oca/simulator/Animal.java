711fd3c624d2575fdba487bcb350e465d043240d
alexylon <euax@icloud.com>
2018-10-26 14:03:59 +0300

    asus

--- src/me/alexandroff/oca/simulator/Animal.java
+++ src/me/alexandroff/oca/simulator/Animal.java
@@ -7,19 +7,41 @@ public interface Animal {
 }
 
 interface Mammal {
-    String getName();
+    default String getName() {
+        return null;
+    }
+
+    String getColor();
 }
 
 abstract class Otter implements Mammal, Animal {
     abstract public String getName();
 }
 
-//A class CANNOT inherit two interfaces that
-//both define default methods with the same signature,
-// unless the class implementing
+//An ABSTRACT class CANNOT inherit two interfaces that both define
+//default methods with the same signature, unless the class implementing
 //the interfaces overrides it with an abstract or concrete method
 
 //If not implemented the two interface methods CANNOT be
 //abstract and default with the same signatute
 
-//Both interface methods CAN be abstract and not implemented
+//It is NOT mandatory to implement abstract interface methods
+//in an ABASTRACT class.
+
+class Fox implements Mammal, Animal {
+    public String getName() {
+        return "Beauty";
+    }
+    //A class CANNOT inherit two interfaces that both define
+//default methods with the same signature, unless the class implementing
+//the interfaces overrides it with a concrete method
+
+//If not implemented the two interface methods CANNOT be
+//abstract and default with the same signatute
+
+    public String getColor() {
+        return "Red";
+    }
+    //All abstract interface methods MUST be
+    //implemented (overriden) in a concrete class
+}
