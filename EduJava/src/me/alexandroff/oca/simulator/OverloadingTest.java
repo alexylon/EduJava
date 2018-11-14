cbdb054cadf73b762266c3b76715aa4040639333
alexylon <euax@icloud.com>
2018-11-14 16:51:15 +0200

    KAME

---
+++ EduJava/src/me/alexandroff/oca/simulator/OverloadingTest.java
@@ -0,0 +1,34 @@
+package me.alexandroff.oca.simulator;
+
+public class OverloadingTest {
+    static void method1(String str) {
+        System.out.println("String");
+    }
+
+    static void method1(Object obj) {
+        System.out.println("Object");
+    }
+
+    static void method2(int intVar) {
+        System.out.println("int");
+    }
+
+    static void method2(Integer integerVar) {
+        System.out.println("Integer");
+    }
+
+    public static void main(String[] args) {
+        method1("word");
+        // Java looks for the most specific type
+        method1(56);
+        // After seeing that there is no an int parameter
+        // Java autoboxes the 'int' and it becomes an object
+
+        method2(56);
+        // Java looks for the most specific type
+        method2(new Integer(5));
+        // Java looks for the most specific type
+    }
+}
+
+
