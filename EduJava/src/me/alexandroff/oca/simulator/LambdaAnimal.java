a6b554594f076fcf3608da79652580e1dda4be3a
alexylon <euax@icloud.com>
2018-11-16 16:38:19 +0200

    KAME

--- EduJava/src/me/alexandroff/oca/simulator/LambdaAnimal.java
+++ EduJava/src/me/alexandroff/oca/simulator/LambdaAnimal.java
@@ -1,6 +1,7 @@
 package me.alexandroff.oca.simulator;
 
 import java.util.*;
+import java.util.function.*;
 
 class Animals {
     private String species;
@@ -38,21 +39,48 @@ class CheckIfHopper implements CheckTrait {
 
 class TraditionalSearch {
     public static void main(String[] args) {
-        List<Animals> animals = new ArrayList<Animals>(); // list of animals
-        animals.add(new Animals("fish", false, true));
-        animals.add(new Animals("kangaroo", true, false));
-        animals.add(new Animals("rabbit", true, false));
-        animals.add(new Animals("turtle", false, true));
+        List<Animals> animals = new ArrayList<Animals>();
+        // list of animals
+        animals.add(new Animals
+                ("fish", false, true));
+        animals.add(new Animals
+                ("kangaroo", true, false));
+        animals.add(new Animals
+                ("rabbit", true, false));
+        animals.add(new Animals
+                ("turtle", false, true));
 
-        System.out.print("Hoppers: ");
+        System.out.println("Hoppers: ");
         print(animals, new CheckIfHopper()); // Without lambda
         // pass class that does check
 
-        System.out.print("Swimmers: ");
+        System.out.println();
+
+        System.out.println("Swimmers: ");
         print(animals, a -> a.canSwim()); // With lambda
+        print(animals, (a) -> a.canSwim());
+        printP(animals, (Animals a) -> {
+            return a.canSwim();
+        });
+        // print(animals, (a) -> {a.canSwim()});
+        // DOES NOT COMPILE. Missing 'return' statement
+        // print(animals, Animals a -> a.canSwim());
+        // DOES NOT COMPILE. Missing parentheses when there is type
     }
 
-    private static void print(List<Animals> animals, CheckTrait checker) {
+    private static void print(List<Animals> animals,
+                              CheckTrait checker) {
+        for (Animals animal : animals) {
+            if (checker.test(animal)) // the general check
+                System.out.print(animal + " ");
+        }
+        System.out.println();
+    }
+
+    // OR with Predicate
+
+    private static void printP(List<Animals> animals,
+                               Predicate<Animals> checker) {
         for (Animals animal : animals) {
             if (checker.test(animal)) // the general check
                 System.out.print(animal + " ");
