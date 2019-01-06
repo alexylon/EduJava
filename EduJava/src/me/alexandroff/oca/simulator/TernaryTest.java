72506113750ccfe7e62ebd2dd62579c6d71dfb28
alex <euax@icloud.com>
2019-01-06 19:58:51 +0200

    MacBook

---
+++ EduJava/src/me/alexandroff/oca/simulator/TernaryTest.java
@@ -0,0 +1,11 @@
+package me.alexandroff.oca.simulator;
+
+public class TernaryTest {
+    public static void main(String[] args) {
+        String tiger = "Tiger";
+        String lion = "Lion";
+        // String result = 10 > 100 ? lion : tiger = "Timur";
+        // NO, it is the same as String result = (10 > 100 ? lion : tiger) = "Timur";
+        // but (10 > 100 ? lion : tiger) is not a variable
+    }
+}
