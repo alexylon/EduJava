71b4e6fcb1b40a1481e18c1f0dda35a943dd2bfe
alexylon <euax@icloud.com>
2019-01-08 13:26:27 +0200

    Irena

--- EduJava/src/me/alexandroff/oca/simulator/Test21.java
+++ EduJava/src/me/alexandroff/oca/simulator/Test21.java
@@ -5,10 +5,13 @@ public class Test21 {
         int counter = 0;
         outer:
         for (int i = 0; i < 3; i++) {
+            System.out.println("i = " + i);
             middle:
             for (int j = 0; j < 3; j++) {
+                System.out.println("j = " + j);
                 inner:
                 for (int k = 0; k < 3; k++) {
+                    System.out.println("k = " + k);
                     if (k - j > 0) {
                         break middle;
                     }
