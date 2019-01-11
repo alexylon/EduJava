c5005ada3127bf69ba602184f751b750157bd178
alexylon <euax@icloud.com>
2019-01-11 16:58:41 +0200

    KAME

---
+++ EduJava/src/me/alexandroff/oca/simulator/ConcreteClass.java
@@ -0,0 +1,40 @@
+package me.alexandroff.oca.simulator;
+
+public class ConcreteClass {
+    public ConcreteClass(int a) {
+        System.out.println("ConcreteClass");
+    }
+
+    public void m1() {
+    }
+}
+
+abstract class AbstractClass extends ConcreteClass {
+    AbstractClass(int m) {
+        super(m);
+        System.out.println("AbstractClass");
+    }
+
+    abstract public void m1();
+    // The overridden method MAY be abstract if in an abstract class
+}
+
+class ConcreteSubClass extends AbstractClass {
+    ConcreteSubClass(int s) {
+        super(s);
+        System.out.println("ConcreteSubClass");
+    }
+
+    public void m1() {
+    }
+}
+
+class TestingClass {
+    public static void main(String[] args) {
+        new ConcreteSubClass(5);
+
+        //new AbstractClass(6);
+        // NO, abstract classes and interfaces CANNOT be instantiated
+
+    }
+}
