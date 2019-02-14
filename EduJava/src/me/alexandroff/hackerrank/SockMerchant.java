a6cc7a6afea42ef3b1e7e220bf74dd33fa94b9ce
alexylon <euax@icloud.com>
2019-02-14 12:58:10 +0200

    KAME

---
+++ EduJava/src/me/alexandroff/hackerrank/SockMerchant.java
@@ -0,0 +1,15 @@
+package me.alexandroff.hackerrank;
+
+//How many pairs are in the array
+
+public class SockMerchant {
+    static int sockMerchant(int n, int[] ar) {
+        int pairs = 0;
+
+        return pairs;
+    }
+
+    public static void main(String[] args) {
+        int[] colors = {};
+    }
+}
